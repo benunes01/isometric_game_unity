8ec36c9a7629e4f6da16b6b26856e0cd9dd2d683
benunes01 <be_nunes_pinheiro@hotmail.com>
2021-03-04 16:14:13 -0300

    feat: ordenando por andares atraves de script

---
+++ isometric_game/Assets/Scripts/Board/Floor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Floor : MonoBehaviour
+{
+    [HideInInspector]
+    public TilemapRenderer tilemapRenderer;
+
+    public int order { 
+        get { 
+            return tilemapRenderer.sortingOrder; 
+            }
+                }
+
+    public int contentOrder;
+
+    void Awake() {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+    }
+}
