a0b647e5fd8177e9ce75846901d251bada49ae07
benunes01 <be_nunes_pinheiro@hotmail.com>
2021-03-15 02:58:03 -0300

    feat: Criação dos scripts para conseguir carregar o mapa de identificar onde está cada Tile

--- isometric_game/Assets/Scripts/Board/Floor.cs
+++ isometric_game/Assets/Scripts/Board/Floor.cs
@@ -15,8 +15,26 @@ public class Floor : MonoBehaviour
                 }
 
     public int contentOrder;
+    public Vector3Int minXY;
+    public Vector3Int maxXY;
+    [HideInInspector]
+    public Tilemap tilemap;
 
     void Awake() {
         tilemapRenderer = GetComponent<TilemapRenderer>();
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    public List<Vector3Int> LoadTiles() {
+        List<Vector3Int> tiles = new List<Vector3Int>();
+        for(int i=minXY.x; i<=maxXY.x; i++) {
+            for(int j=minXY.y; j<=maxXY.y; j++) {
+                Vector3Int currentPos = new Vector3Int(i, j, 0);
+                if(tilemap.HasTile(currentPos)) {
+                    tiles.Add(currentPos);
+                }
+            }
+        }
+        return tiles;
     }
 }
